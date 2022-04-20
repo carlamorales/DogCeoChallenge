d0354d8e24ffcf9e8583aec0a2a00b3b27b3ff7d
carlamorales <carlamoralespacheco@gmail.com>
2022-04-20 18:34:15 -0400

    se pinta tabla provisoria

--- DogCeoChallenge/BreedsListViewController.swift
+++ DogCeoChallenge/BreedsListViewController.swift
@@ -8,11 +8,33 @@
 import UIKit
 
 class BreedsListViewController: UIViewController {
+    
+    let breedsListTable = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "DOG BREEDS"
+        
+        view.addSubview(breedsListTable)
+        breedsListTable.pin(to: view)
+        
+        breedsListTable.dataSource = self
+        breedsListTable.delegate = self
     }
 
 }
+
+extension BreedsListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+}
+
+extension BreedsListViewController: UITableViewDelegate {}
---
+++ DogCeoChallenge/UIViewExtension.swift
@@ -0,0 +1,22 @@
+//
+//  UIViewExtension.swift
+//  DogCeoChallenge
+//
+//  Created by Mobdev on 20-04-22.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func pin(to superView: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.topAnchor),
+            leadingAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.leadingAnchor),
+            trailingAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.trailingAnchor),
+            bottomAnchor.constraint(equalTo: superView.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
+    
+}
